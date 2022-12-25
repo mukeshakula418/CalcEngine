8fd64dd29eb256d9a72449b55a6b81adf014255e
Mukesh A <70726140+mukeshakula418@users.noreply.github.com>
2022-12-25 14:22:56 -0600

    Implemented custom exception by creating an new exception class extending super class

---
+++ src/com/mukesh/calcengine/InvalidStatementException.java
@@ -0,0 +1,7 @@
+package com.mukesh.calcengine;
+
+public class InvalidStatementException extends Exception {
+    public InvalidStatementException(String message) {
+        super(message);
+    }
+}
